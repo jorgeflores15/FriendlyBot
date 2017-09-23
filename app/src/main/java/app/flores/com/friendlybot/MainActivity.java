66771b1950c0885db45b5ee8402c2c25dbf15d7f
alejandro <victor.saico@tecsup.edu.pe>
2017-09-23 18:16:48 -0500

    sprint2

--- app/src/main/java/app/flores/com/friendlybot/MainActivity.java
+++ app/src/main/java/app/flores/com/friendlybot/MainActivity.java
@@ -2,22 +2,23 @@ package app.flores.com.friendlybot;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
 import com.ibm.mobilefirstplatform.clientsdk.android.analytics.api.Analytics;
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Response;
@@ -207,9 +208,13 @@ public class MainActivity extends AppCompatActivity {
                 recordMessage();
             }
         });
-    }
+    };
 
-    ;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     // Speech-to-Text Record Audio permission
     @Override
